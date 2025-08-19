8500186c7d48cd0197c21f668b17693bc7e7f02c
dantasaline <alinedantasdl@gmail.com>
2025-08-19 09:01:51 -0300

    .

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
 import express from 'express'
 
+import pkg from '@prisma/client';
+const { PrismaClient } = pkg;
+const prisma = new PrismaClient();
+
 const app = express()
 app.use(express.json())
-const usuarios = []
 
 //criar as rotas
-app.get('/cadastro',(req,res)=>{
-    res.status(200).json(usuarios)
+app.get('/cadastro', async (req,res)=>{
+
+    const lista_usuarios = await prisma.usuario.findMany()
+
+    res.status(200).json(lista_usuarios)
 })
 
-app.post('/cadastro',(req,res)=>{
-    //console.log(req.body)
-    usuarios.push(req.body)
+app.post('/cadastro', async (req,res)=>{
+    
+    await prisma.usuario.create({
+        data:{
+            email: req.body.email,
+            nome: req.body.nome,
+            idade: req.body.idade
+        }
+    })
+
     res.status(201) .json(req.body)
 })
 
 //configurar porta servidor 
 app.listen(3000, ()=>(console.log('SERVIDOR RODANDO')))
+
