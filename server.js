54b7d7aab664dbe4a1816aff384a30758a80e475
dantasaline <alinedantasdl@gmail.com>
2025-09-23 14:57:47 -0300

    .

--- server.js
+++ server.js
@@ -1,62 +1,47 @@
 import express from 'express'
+import cors from "cors"
 
-import pkg from '@prisma/client';
-const { PrismaClient } = pkg;
-const prisma = new PrismaClient();
+import pkg from '@prisma/client'
+const { PrismaClient } = pkg
+const prisma = new PrismaClient()
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
-//criar as rotas
-app.get('/cadastro', async (req,res)=>{
-
-    const lista_usuarios = await prisma.usuario.findMany()
-
-    res.status(200).json(lista_usuarios)
+app.get('/cadastro', async (req, res) => {
+  const lista_usuarios = await prisma.usuario.findMany()
+  res.status(200).json(lista_usuarios)
 })
 
-app.post('/cadastro', async (req,res)=>{
-    
-    await prisma.usuario.create({
-        data:{
-            email: req.body.email,
-            nome: req.body.nome,
-            idade: req.body.idade
-        }
-    })
-
-    res.status(201) .json(req.body)
+app.post('/cadastro', async (req, res) => {
+  const usuario = await prisma.usuario.create({
+    data: {
+      nome: req.body.nome,
+      idade: req.body.idade,
+      email: req.body.email
+    }
+  })
+  res.status(201).json(usuario)
 })
 
-//atualizar os clientes
-app.put('/cadastro/:id', async (req,res)=>{
-
-    // console.log(req.params.id)
-    await prisma.usuario.update({
-        where:{
-            id:req.params.id
-        },
-        data:{
-            email: req.body.email,
-            nome: req.body.nome,
-            idade: req.body.idade
-        }
-    })
-    res.status(201).json({"message":"Usuario Atualizado"})
+app.put('/cadastro/:id', async (req,res) => {
+  const usuario = await prisma.usuario.update({
+    where: { id: req.params.id },
+    data: {
+      nome: req.body.nome,
+      idade: req.body.idade,
+      email: req.body.email
+    }
+  })
+  res.status(200).json(usuario)
 })
 
-
-app.delete('/cadastro/:id', async (req,res)=>{
-
-    // console.log(req.params.id)
-    await prisma.usuario.delete({
-        where:{
-            id:req.params.id
-        }
-    })
-    res.status(201).json({"message":"Usuario Deletado"})
+app.delete('/cadastro/:id', async (req, res) => {
+  await prisma.usuario.delete({
+    where: { id: req.params.id }
+  })
+  res.status(200).json({ message: "Usuário Deletado" })
 })
 
-//configurar porta servidor 
-app.listen(3000, ()=>(console.log('SERVIDOR RODANDO')))
-
+app.listen(3000, () => console.log('SERVIDOR RODANDO'))
